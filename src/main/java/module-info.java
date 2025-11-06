68ad1db79bf183577013fa68332ac79c49630424
Oluwajomiloju Wusu <134692110+JomiWusu@users.noreply.github.com>
2025-11-05 23:18:03 -0500

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.specialprojectreal {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.specialprojectreal to javafx.fxml;
-    exports com.example.specialprojectreal;
-}
